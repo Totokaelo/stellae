ec11d36344d64f9b6af64a628a7d08edf3e23cbc
Quinton Harris <quinton.harris@gmail.com>
2015-05-30 22:52:31 -0700

    Add Order documentation

--- lib/stellae/types/order.rb
+++ lib/stellae/types/order.rb
@@ -3,18 +3,46 @@ module Stellae
     class Order < Base
       root_name :ohn
 
+      # Shipping Carrier
+      # 'Preferred carrier'
+      # ex: either FEDEX or UPS
+      #
       string :carrier
+
+      # Shipping Service
+      # 'Carrier service code' ... 'The service code is passed to the carrier'
+      # Numeric service id
+      # ex: '01' (FEDEX PRIORITY OVERNIGHT)
+      #
+      string :service
+
+      # Currency
+      # 'If currency is not filled in, it will be based on
+      # system parameters internal to the Stellae system.'
+      # For now, I think leave this alone and it'll default to USD
+      #
       string :currency
 
       string :customer_address_1
       string :customer_address_2
       string :customer_address_3
       string :customer_address_city
+
+      # Customer Billing Address Country
+      # ISO2
+      #
       string :customer_address_country
       string :customer_address_state
       string :customer_address_zip
 
+      # Customer Name
+      # 'Bill to customer name'
+      #
       string :customer_name
+
+      # Customer Code
+      # 'Bill to customer code – if blank, will assign'
+      #
       string :customer_code
       string :customer_po
       string :customer_telephone
@@ -23,6 +51,9 @@ module Stellae
       string :delivery_address_2
       string :delivery_address_3
       string :delivery_address_city
+      # Customer Shipping Address Country
+      # ISO2
+      #
       string :delivery_address_country
       string :delivery_address_state
       string :delivery_address_zip
@@ -36,10 +67,19 @@ module Stellae
       string :delivery_telephone
       string :delivery_to
 
+      # Discount
+      # 'total discount $'
+      #
       decimal :discount
 
+      # Email
+      # 'used to send to fedex/ups'
+      #
       string :email
 
+      # Freight Account
+      # "On bill to receipt, recipient’s account"
+      #
       string :freight_account
 
       decimal :misc1
@@ -48,6 +88,11 @@ module Stellae
       decimal :misc2
       string :misc2_reason
 
+      # Order Id
+      # 'Order number (sending system order number).  Stellae will store this
+      # information and have use a different order number internally'
+      # Our native Order Id
+      #
       string :order_id
 
       # Must be "OO" or "CM"
@@ -55,13 +100,13 @@ module Stellae
       # CM: Order Return
       string :order_type
 
-      string :service
-
       decimal :shipping_fees
       decimal :taxes
       decimal :total_amount
 
-      # URLencoded Invoice Url
+      # User1
+      # URLencoded Url pointing to an invoice PDF
+      #
       string :user1
       string :user2
       string :user3
