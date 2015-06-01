b36ef21b18761fe7894f13a75c5c305e998d3597
Quinton Harris <quinton.harris@gmail.com>
2015-06-01 12:53:23 -0700

    Get our damn credentials out of the public eye

--- spec/stellae_spec_helper.rb
+++ spec/stellae_spec_helper.rb
@@ -2,8 +2,8 @@ module StellaeSpecHelper
   def gateway
     Stellae::Gateway.new(
       endpoint_url: 'https://webservice.stellae.us/SIIServices_multi/Siiservice.svc',
-      username: 'TOTOKAELO',
-      password: 'fP4#WNZtu5Gf',
+      username: ENV['STELLAE_USERNAME'],
+      password: ENV['STELLAE_PASSWORD'],
       logger: Logger.new(STDOUT)
     )
   end
